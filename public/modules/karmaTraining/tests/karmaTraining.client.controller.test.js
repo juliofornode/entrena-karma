eafa51477cb21f29afbe7e816ab6a281ca0bae5d
Julio Colomer <juliocolomer@julios-air.att.net>
2016-02-26 18:03:13 -0800

    minor changes

--- public/modules/karmaTraining/tests/karmaTraining.client.controller.test.js
+++ public/modules/karmaTraining/tests/karmaTraining.client.controller.test.js
@@ -4,16 +4,24 @@
 
     describe('ControllerTestingControllers', function () {
 
+        beforeEach(module('karmaTraining'));
+
+        var $controller;
+
+        beforeEach(inject(function(_$controller_){
+            // The injector unwraps the underscores (_) from around the parameter
+            // names when matching
+            $controller = _$controller_;
+        }));
+
         it('should give us the palabra', function () {
 
+            var scope = {};
+            var controller = $controller('ControllerTestingControllers', { $scope: scope });
             expect(scope.palabra).toBe("Caraculo");
 
         })
 
-
-
     })
-
-
 
 }());
